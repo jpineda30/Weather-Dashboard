1f5374e2c3e6dc664981b9a44a6e40fd1d45dbb9
pepe30 <jose.pinedaesquer@gmail.com>
2020-11-07 22:06:01 -0600

    corrected successfully click event for dom

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ $(document).ready(function() {
 
         for(city of cities)
         {
-            var add = $("<div>");
+            var add = $("<button>");
             add.text(city);
-            add.addClass("card p-2 btnCity");
+            add.addClass("card p-2 btnCity w-100");
             add.attr("data-name",city);
             $("#menu").append(add);
         }
@@ -40,109 +40,123 @@ $(document).ready(function() {
 
 //functions
 
+
 function setWeather(name)
 {
     
-    $("#main").css("visibility","initial");
-    $("#forecast").css("visibility","initial");
+        $("#main").css("visibility","initial");
+        $("#forecast").css("visibility","initial");
     
-    if(name != "")
-        {
+         if(name != "")
+         {
             var city = name;
             
             var urlNow = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" + key;
             
             $.ajax({
 
-                url:urlNow,
-                method:"GET"
+                        url:urlNow,
+                        method:"GET"
     
-            }).then(function(response){
+                     }).then(function(response){
                 
-                localStorage.setItem("lastConsult",response.name);
-                var Mdate = response.dt;
-                var Mdt = new Date(Mdate * 1000);
-                var mainDate = moment(Mdt).format("DD/MM/YYYY");
+                        localStorage.setItem("lastConsult",response.name);
+                        var Mdate = response.dt;
+                        var Mdt = new Date(Mdate * 1000);
+                        var mainDate = moment(Mdt).format("DD/MM/YYYY");
 
-                $("#city").text(response.name + " ("+ mainDate +")");
-                var icon = "https://openweathermap.org/img/wn/"+ response.weather[0].icon+"@2x.png";
-                $("#mainIcon").attr("src",icon);
+                        $("#city").text(response.name + " ("+ mainDate +")");
+                        var icon = "https://openweathermap.org/img/wn/"+ response.weather[0].icon+"@2x.png";
+                        $("#mainIcon").attr("src",icon);
 
-                var kelvin = 273.15;
-                var temp = 32 + (9/5 * (response.main.temp - kelvin));
-                $("#temperature").text("Temperature: " + temp.toFixed(2) + " °F");
-                var windSpeed = response.wind.speed*2.237;
-                $("#humidity").text("Humidity: " + response.main.humidity + "%");
-                $("#wind").text("Wind speed: " + windSpeed.toFixed(2) + " MPH");
-                $("#uv-index").text("0");
+                        var kelvin = 273.15;
+                        var temp = 32 + (9/5 * (response.main.temp - kelvin));
+                        $("#temperature").text("Temperature: " + temp.toFixed(2) + " °F");
+                        var windSpeed = response.wind.speed*2.237;
+                        $("#humidity").text("Humidity: " + response.main.humidity + "%");
+                        $("#wind").text("Wind speed: " + windSpeed.toFixed(2) + " MPH");
+                        $("#uv-index").text("0");
                
                 //save city
-                var current = JSON.parse(localStorage.getItem("CitiesFile"));
-                if(current.indexOf(response.name) == -1)
-                {
-                    
-                    current.push(response.name);
-                    localStorage["CitiesFile"] = JSON.stringify(current);
+                        var current = JSON.parse(localStorage.getItem("CitiesFile"));
+                        if(current.indexOf(response.name) == -1)
+                        {
+                            
+                            current.push(response.name);
+                            localStorage["CitiesFile"] = JSON.stringify(current);
+                            current = JSON.parse(localStorage.getItem("CitiesFile"));
 
-                    var add = $("<div>");
-                    add.text(response.name);
-                    add.addClass("card p-2 btnCity");
-                    add.attr("data-name",response.name);
-                    $("#menu").append(add);
-
-
-                }
-                else
-                {
-                    
-                }
-
-                //retrive forecast
-                var lat = response.coord.lat;
-                var long = response.coord.lon;
-
-                //retrive uv index
-                var uvindex = "https://api.openweathermap.org/data/2.5/uvi?lat="+lat+ "&lon="+long+"&appid="+key;
-                $.ajax({
-
-                    url:uvindex,
-                    method:"GET"
+                            var add = $("<button>");
+                            add.text(response.name);
+                            add.addClass("card p-2 btnCity w-100");
+                            add.addClass("active");
+                            add.attr("data-name",response.name);
+                            //add onclick
+                            add.on("click",function(){
+                                
         
-                }).then(function(uvresponse){
-                    $("#uv-index").text(uvresponse.value);
+                                    var esto = $(this).attr("data-name");
+                                    console.log(esto);
+                                    setWeather(esto);
+                            
+                                
+                            });
+                            //
+                            $("#cities").append(add);
 
-                    if(uvresponse.value<=2)
-                    { 
-                        $("#uv-index").removeClass();
-                        $("#uv-index").addClass("bg-success h6 p-2 rounded-pill");
-                    }
-                    else 
-                    {
-                        if(uvresponse.value<=5)
-                        {
-                            $("#uv-index").removeClass();
-                            $("#uv-index").addClass("bg-warning h6 p-2 rounded-pill");
-                        }
-                        else if(uvresponse.value<=7)
-                        {
-                            $("#uv-index").removeClass();
-                            $("#uv-index").addClass("bg-orange h6 p-2 rounded-pill");
+
                         }
                         else
                         {
-                            $("#uv-index").removeClass();
-                            $("#uv-index").addClass("bg-danger h6 p-2 rounded-pill");
+                            
                         }
 
-                    }
-                   
-                   
-                });
+                        //retrive forecast
+                        var lat = response.coord.lat;
+                        var long = response.coord.lon;
+
+                        //retrive uv index
+                        var uvindex = "https://api.openweathermap.org/data/2.5/uvi?lat="+lat+ "&lon="+long+"&appid="+key;
+                        $.ajax({
+
+                            url:uvindex,
+                            method:"GET"
+                
+                        }).then(function(uvresponse){
+                            $("#uv-index").text(uvresponse.value);
+
+                            if(uvresponse.value<=2)
+                            { 
+                                $("#uv-index").removeClass();
+                                $("#uv-index").addClass("bg-success h6 p-2 rounded-pill");
+                            }
+                            else 
+                            {
+                                if(uvresponse.value<=5)
+                                {
+                                    $("#uv-index").removeClass();
+                                    $("#uv-index").addClass("bg-warning h6 p-2 rounded-pill");
+                                }
+                                else if(uvresponse.value<=7)
+                                {
+                                    $("#uv-index").removeClass();
+                                    $("#uv-index").addClass("bg-orange h6 p-2 rounded-pill");
+                                }
+                                else
+                                {
+                                    $("#uv-index").removeClass();
+                                    $("#uv-index").addClass("bg-danger h6 p-2 rounded-pill");
+                                }
+
+                            }
+                        
+                        
+                        });
                 
 
 
                 //get forecast
-                var urlForecast = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+long+"&exclude={part}&appid=" + key ;
+                       var urlForecast = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+long+"&exclude={part}&appid=" + key ;
                 
                 //set today
 
@@ -152,7 +166,7 @@ function setWeather(name)
                     url:urlForecast,
                     method:"GET"
         
-                }).then(function(forecast){
+                 }).then(function(forecast){
                     
                     
                     var daysForcast = forecast.daily;
@@ -197,14 +211,13 @@ function setWeather(name)
                     }
 
 
-                })
+                });
                 
 
             });
 
         }
-        else
-        {}
+        
 }
 
 
@@ -213,6 +226,7 @@ function setWeather(name)
     $(".btnCity").on("click",function(){
         
         var esto = $(this).attr("data-name");
+        console.log(esto);
         setWeather(esto);
 
     });
@@ -249,5 +263,5 @@ function setWeather(name)
        
     })
     
-
+ 
 })
